797b6b479e32a232c0516793d3efe97fbedb6e4b
Samuel <samuel@mknd-records.com>
2024-03-21 15:15:21 +0100

    Mise à jour des paramètres de l'application dans .devcontainer/devcontainer.json et docker-compose.yml, suppression des scripts de création de la base de données dans docker/mysql/create-appy-database.sh et docker/mysql/create-testing-database.sh, et mise à jour des ports forwardés dans .devcontainer/devcontainer.json

--- docker/mysql/create-appy-database.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-mysql --user=root --password="$MYSQL_ROOT_PASSWORD" <<-EOSQL
-    CREATE DATABASE IF NOT EXISTS appY;
-    GRANT ALL PRIVILEGES ON \`appY%\`.* TO '$MYSQL_USER'@'%';
-EOSQL
