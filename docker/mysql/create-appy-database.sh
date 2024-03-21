57129d39d4aeb8078115e444151685fc7b71ac27
Samuel <samuel@mknd-records.com>
2024-03-21 14:04:09 +0100

    Mise à jour des paramètres de l'application dans .env.example et du script de création de la base de données dans docker/mysql/create-appy-database.sh

--- docker/mysql/create-appy-database.sh
+++ docker/mysql/create-appy-database.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 mysql --user=root --password="$MYSQL_ROOT_PASSWORD" <<-EOSQL
-    CREATE DATABASE IF NOT EXISTS appy;
-    GRANT ALL PRIVILEGES ON \`appy%\`.* TO '$MYSQL_USER'@'%';
+    CREATE DATABASE IF NOT EXISTS appY;
+    GRANT ALL PRIVILEGES ON \`appY%\`.* TO '$MYSQL_USER'@'%';
 EOSQL
