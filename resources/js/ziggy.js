ba6086623113d277df847d1192d1d603a997283e
Samuel <60432398+kuasar-mknd@users.noreply.github.com>
2024-03-22 12:00:34 +0000

    Mise à jour des dépendances Composer et du fichier phpstan.neon

---
+++ resources/js/ziggy.js
@@ -0,0 +1,25 @@
+const Ziggy = {
+    url: "http://localhost",
+    port: null,
+    defaults: {},
+    routes: {
+        "ignition.healthCheck": {
+            uri: "_ignition/health-check",
+            methods: ["GET", "HEAD"],
+        },
+        "ignition.executeSolution": {
+            uri: "_ignition/execute-solution",
+            methods: ["POST"],
+        },
+        "ignition.updateConfig": {
+            uri: "_ignition/update-config",
+            methods: ["POST"],
+        },
+    },
+};
+
+if (typeof window !== "undefined" && typeof window.Ziggy !== "undefined") {
+    Object.assign(Ziggy.routes, window.Ziggy.routes);
+}
+
+export { Ziggy };
